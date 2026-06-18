a69ae47ed963e8d3c406fce073abacf6af3799b7
skpaek <skpaek@zenithcloud.com>
2026-06-18 13:37:29 +0900

    260618-prd_deploy-4

--- 99. Deploy Resources.ps1
+++ 99. Deploy Resources.ps1
@@ -2144,9 +2144,9 @@ function Resolve-DataDiskSku {
     switch ($InputValue.Trim()) {
         'StandardSSD_LRS' { return 'StandardSSD_LRS' }
         'StandardSSD' { return 'StandardSSD_LRS' }
-        'PremiumSSD_LRS'  { return 'PremiumSSD_LRS' }
-        'Premium'         { return 'PremiumSSD_LRS' }
-        'PremiumSSD'      { return 'PremiumSSD_LRS' } # (선택) 사용자가 이렇게 입력할 수도 있으니 대비
+        'PremiumSSD_LRS'  { return 'Premium_LRS' }
+        'Premium'         { return 'Premium_LRS' }
+        'PremiumSSD'      { return 'Premium_LRS' } # (선택) 사용자가 이렇게 입력할 수도 있으니 대비
         default { throw "Only StandardSSD_LRS or PremiumSSD_LRS is allowed. Input: $InputValue" }
     }
 }
